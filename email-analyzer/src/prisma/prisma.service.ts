30b79042ce4850958cc4b1526cd20aca08f22e4c
Gustavo Szesz <szeszgustavo@gmail.com>
2026-04-03 19:40:23 -0300

    (fix) -> Prisma running

--- email-analyzer/src/prisma/prisma.service.ts
+++ email-analyzer/src/prisma/prisma.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is required');
+    }
+
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
